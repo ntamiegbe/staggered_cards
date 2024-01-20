1944bf87f6f991fef41f3c6a1fa28b4d259c7984
Ntami Egbe <ntamiegbe00@gmail.com>
2024-01-21 00:30:05 +0100

    feat: added animation for second card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,15 +36,41 @@ const Home = () => {
             className="w-20 h-20 bg-stone-100 rounded-lg"
             initial={{ opacity: 0, y: 80 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, ease: "easeInOut", delay: 0.3 }}>
-          </motion.div>
+            transition={{
+              duration: 1,
+              ease: "easeInOut",
+              delay: 0.3,
+            }}
+          />
           <motion.div
             className="w-20 h-20 bg-stone-100 rounded-full"
             initial={{ opacity: 0, y: -80 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}></motion.div>
+            transition={{
+              duration: 1,
+              ease: "easeInOut",
+              delay: 0.5,
+            }}
+          />
         </motion.div>
-        <motion.div variants={gridSquareVariants} className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"></motion.div>
+
+        <motion.div variants={gridSquareVariants} className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10">
+          <motion.div
+            className="w-1/3 h-1/3 shadow-md bg-rose-500"
+            animate={{
+              scale: [1, 2, 2, 1],
+              rotate: [0, 90, 90, 0],
+              borderRadius: ["10%", "10%", "50%", "10%"]
+            }}
+            transition={{
+              duration: 5,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatDelay: 1
+            }}
+          />
+        </motion.div>
+
         <motion.div variants={gridSquareVariants} className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"></motion.div>
         <motion.div variants={gridSquareVariants} className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"></motion.div>
         <motion.div variants={gridSquareVariants} className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10"></motion.div>
